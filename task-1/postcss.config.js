cc9ff2ec900d193d236ac10de7c0040cf9b4755a
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-05 22:22:35 +0700

    Add first prototype of header and footer for index page

--- task-1/postcss.config.js
+++ task-1/postcss.config.js
@@ -1,8 +1,14 @@
 module.exports = {
 	plugins: {
-		'postcss-url': {
-			url: 'rebase'
-		},
+		'postcss-url':[
+			{ filter: '**/fonts/*', url: 'rebase' },
+			{
+				filter: '**/icons/*',
+				basePath: __dirname,
+				url: 'inline',
+				encodeType: 'base64'
+			},
+		],
 		'postcss-preset-env': {}
 	}
 };
