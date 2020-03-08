04e56ee2276c56651d65aed743aa2342ae8da55f
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-08 18:11:46 +0700

    Add styles for start page

--- task-1/postcss.config.js
+++ task-1/postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
 		'postcss-url':[
 			{ filter: '**/fonts/*', url: 'rebase' },
 			{
-				filter: '**/icons/*',
 				basePath: __dirname,
 				url: 'inline',
 				encodeType: 'base64'
