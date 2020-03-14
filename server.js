b06d5241910236e29fc00e6d87de1832bcb944e2
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-14 23:40:18 +0700

    Add swagger ui page

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const app = express();
 const configureApi = require('./server/configure-api');
+const configureSwagger = require('./server/configure-swagger');
 
 // В данный момент у нас две папки со статическими файлами, одна генерируется webpack, вторая хранит html.
 app.use(express.static(path.resolve(__dirname, 'dist')));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 configureApi(app);
+configureSwagger(app);
 
 app.listen(process.env.PORT || 3000);
