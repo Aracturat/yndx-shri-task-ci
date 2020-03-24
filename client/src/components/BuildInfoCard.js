f8db34707a62255ed18e4ed9dfe291d197d23893
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-25 03:22:37 +0700

    Add TextWithIcon component, update BuildInfoCard component

--- client/src/components/BuildInfoCard.js
+++ client/src/components/BuildInfoCard.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { bemHelper } from '../bem-helper';
 import { Icon } from './Icon';
+import { TextWithIcon } from './TextWithIcon';
 
 const cn = bemHelper('build-info-card');
 
 export function BuildInfoCard({ buildInfoToBottom, withHover, tag = 'div', className = '' }) {
 	let WrapperTag = tag;
 
+	let config = {
+		status: 'success',
+		buildNumber: '1368',
+		commitName: 'add documentation for postgres scaler',
+		commitBranch: 'master',
+		commitHash: '9c9f0b9',
+		commitAuthor: 'Philip Kirkorov',
+		buildDate: '21 янв, 03:06',
+		buildDuration: '1 ч 20 мин'
+	};
+
 	return (
-		<WrapperTag className={cn(null, { 'build-info-to-bottom': buildInfoToBottom, 'with-hover': withHover }, className)}>
-			<Icon name="success" className="build-info-card__status-icon" />
-			<div className="build-info-card__commit-first-line">
-				<div className="build-info-card__commit-number build-info-card__commit-number--success">#1368</div>
-				<div className="build-info-card__commit-name">add documentation for postgres scaler</div>
+		<WrapperTag className={
+			cn(null, {
+				'build-info-to-bottom': buildInfoToBottom,
+				'with-hover': withHover
+			}, className)}
+		>
+			<Icon name={config.status} className={cn('status-icon')} />
+			<div className={cn('commit-first-line')}>
+				<div className={cn('commit-number', { 'success': config.status === 'success' })}>#{config.buildNumber}</div>
+				<div className={cn('commit-name')}>{config.commitName}</div>
 			</div>
-			<div className="build-info-card__commit-second-line">
-				<div className="text-with-icon">
-					<Icon name="branch" size="s" className="text-with-icon__icon" />
-					<div className="text-with-icon__primary-text">master</div>
-					<div className="text-with-icon__secondary-text">9c9f0b9</div>
-				</div>
-				<div className="text-with-icon build-info-card__author">
-					<Icon name="person" size="s" className="text-with-icon__icon" />
-					<div className="text-with-icon__primary-text">Philip Kirkorov</div>
-				</div>
+			<div className={cn('commit-second-line')}>
+				<TextWithIcon icon="branch" primary={config.commitBranch} secondary={config.commitHash} />
+				<TextWithIcon icon="person" primary={config.commitAuthor} />
 			</div>
-			<div className="build-info-card__build-info">
-				<div className="text-with-icon build-info-card__build-date">
-					<Icon name="calendar" size="s" className="text-with-icon__icon" />
-					<div className="text-with-icon__secondary-text">21 янв, 03:06</div>
-				</div>
-				<div className="text-with-icon">
-					<Icon name="timer" size="s" className="text-with-icon__icon" />
-					<div className="text-with-icon__secondary-text">1 ч 20 мин</div>
-				</div>
+			<div className={cn('build-info')}>
+				<TextWithIcon icon="calendar" secondary={config.buildDate} />
+				<TextWithIcon icon="timer" secondary={config.buildDuration} />
 			</div>
 		</WrapperTag>
 	)
---
+++ client/src/components/TextWithIcon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { bemHelper } from '../bem-helper';
+import { Icon } from './Icon';
+
+const cn = bemHelper('text-with-icon');
+
+export function TextWithIcon({ icon, primary, secondary, className }) {
+	return (
+		<div className={cn(null, null, className)}>
+			<Icon name={icon} size="s" className={cn('icon')} />
+			{primary && <div className={cn('primary-text')}>{primary}</div>}
+			{secondary && <div className={cn('secondary-text')}>{secondary}</div>}
+		</div>
+	)
+}
