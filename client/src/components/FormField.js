24a0e50f9649f0d1a82b2d73c29fd84d4c53f357
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-25 03:40:03 +0700

    Add FormField component

---
+++ client/src/components/FormField.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { bemHelper } from '../bem-helper';
+
+const cn = bemHelper('form-field');
+
+let id = 0;
+
+export function FormField(
+	{
+		value,
+		onChange,
+		label,
+		afterElement,
+		required = false,
+		inline = false,
+		placeholder,
+		className
+	}
+) {
+	const inputId = `form-field-${id++}`;
+
+	const [inputValue, setInputValue] = useState(value);
+
+	const handleChange = (event) => {
+		setInputValue(event.target.value);
+		onChange && onChange(event.target.value);
+	};
+
+	return (
+		<div className={cn(null, { required, inline }, className)}>
+			<label className={cn('label')} htmlFor={inputId}>{label}</label>
+			<input className={cn('input')}
+				type="text"
+				placeholder={placeholder}
+				id={inputId}
+				defaultValue={value}
+				onChange={handleChange}
+			/>
+			{inputValue && <span className={cn('clear-button')} />}
+			{afterElement && <span className={cn('after-element')}>{afterElement}</span>}
+		</div>
+	)
+}
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { Button } from '../components/Button';
+import { FormField } from '../components/FormField';
 
 export function Settings() {
 	return (
@@ -17,37 +18,12 @@ export function Settings() {
 					<div className="settings-page__form">
 						<h2 className="header-text header-text--small settings-page__header">Settings</h2>
 						<div className="text text--size-s settings-page__description">Configure repository connection and synchronization settings.</div>
-						<div className="form-field form-field--required">
-							<label className="form-field__label" htmlFor="repository">Github repository</label>
-							<input className="form-field__input"
-								type="text"
-								placeholder="user-name/repo-name"
-								id="repository"
-							/>
-						</div>
-						<div className="form-field">
-							<label className="form-field__label" htmlFor="command">Build command</label>
-							<input className="form-field__input"
-								type="text"
-								value="npm ci && npm run build"
-								id="command"
-							/>
-							<span className="form-field__clear-button"></span>
-						</div>
-						<div className="form-field">
-							<label className="form-field__label" htmlFor="branch">Main branch</label>
-							<input className="form-field__input" type="text" value="master" id="branch" />
-							<span className="form-field__clear-button"></span>
-						</div>
-						<div className="form-field form-field--inline">
-							<label className="form-field__label" htmlFor="minutes">Synchronize every</label>
-							<input className="form-field__input settings-page__minutes-input"
-								type="text"
-								value="10"
-								id="minutes"
-							/>
-							<span className="form-field__after-element">minutes</span>
-						</div>
+
+						<FormField required label="Github repository" placeholder="user-name/repo-name" />
+						<FormField value="npm ci && npm run build" label="Build command" />
+						<FormField value="master" label="Main branch" />
+						<FormField inline value="10" label="Synchronize every" afterElement="minutes"/>
+
 						<div className="settings-page__buttons">
 							<Button action className="settings-page__button">Save</Button>
 							<Button className="settings-page__button">Cancel</Button>
