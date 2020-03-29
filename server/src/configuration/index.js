b74eef26607de24dc714a57088e46673cc66e85d
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-03-29 22:03:24 +0700

    Remove hosting of static files

--- server/src/configuration/index.js
+++ server/src/configuration/index.js
@@ -1,11 +1,9 @@
 const configureApi = require('./configure-api');
 const configureSwagger = require('./configure-swagger');
-const configureStatic = require('./configure-static');
 
 function configure(app) {
 	configureApi(app);
 	configureSwagger(app);
-	configureStatic(app);
 }
 
 module.exports = configure;
